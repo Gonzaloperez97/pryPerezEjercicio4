21f3d0e31a9bbb011a7df045944f32cc7a5a7df7
gonzaloperez97 <gonzi.bernardo@gmail.com>
2025-11-03 14:24:11 -0300

    codigo terminado.

--- pryPerezEjercicio4/Form1.Designer.cs
+++ pryPerezEjercicio4/Form1.Designer.cs
@@ -29,20 +29,29 @@
         private void InitializeComponent()
         {
             dgvVentas = new DataGridView();
+            colMozos = new DataGridViewTextBoxColumn();
+            colComida = new DataGridViewTextBoxColumn();
+            colBebidaSin = new DataGridViewTextBoxColumn();
+            colBebidas = new DataGridViewTextBoxColumn();
+            colPostres = new DataGridViewTextBoxColumn();
             btnCerrar = new Button();
             btnValidar = new Button();
-            MozoDelDia = new Button();
+            btnMozoDelDia = new Button();
             btnTotales = new Button();
             lblImportesVentas = new Label();
             lblMozoDelDia = new Label();
             lblTotalGeneral = new Label();
             lblMozoResultado = new Label();
             txtTotalGeneral = new TextBox();
-            colMozos = new DataGridViewTextBoxColumn();
-            colComida = new DataGridViewTextBoxColumn();
-            colBebidaSin = new DataGridViewTextBoxColumn();
-            colBebidas = new DataGridViewTextBoxColumn();
-            colPostres = new DataGridViewTextBoxColumn();
+            lblTotalComidas = new Label();
+            label2 = new Label();
+            lblBebidasSinAlcohol = new Label();
+            lblPostres = new Label();
+            lblBebidasConAlcohol = new Label();
+            txtTotalComidas = new TextBox();
+            txtTotalBebidasCon = new TextBox();
+            txtTotalBebidasSin = new TextBox();
+            txtTotalPostres = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dgvVentas).BeginInit();
             SuspendLayout();
             // 
@@ -52,90 +61,10 @@
             dgvVentas.AllowUserToDeleteRows = false;
             dgvVentas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvVentas.Columns.AddRange(new DataGridViewColumn[] { colMozos, colComida, colBebidaSin, colBebidas, colPostres });
-            dgvVentas.Location = new Point(21, 43);
+            dgvVentas.Location = new Point(12, 23);
             dgvVentas.Name = "dgvVentas";
             dgvVentas.Size = new Size(521, 256);
             dgvVentas.TabIndex = 0;
-            // 
-            // btnCerrar
-            // 
-            btnCerrar.Location = new Point(21, 390);
-            btnCerrar.Name = "btnCerrar";
-            btnCerrar.Size = new Size(75, 23);
-            btnCerrar.TabIndex = 1;
-            btnCerrar.Text = "Cerrar";
-            btnCerrar.UseVisualStyleBackColor = true;
-            // 
-            // btnValidar
-            // 
-            btnValidar.Location = new Point(161, 390);
-            btnValidar.Name = "btnValidar";
-            btnValidar.Size = new Size(75, 23);
-            btnValidar.TabIndex = 2;
-            btnValidar.Text = "Validar Datos";
-            btnValidar.UseVisualStyleBackColor = true;
-            // 
-            // MozoDelDia
-            // 
-            MozoDelDia.Location = new Point(306, 390);
-            MozoDelDia.Name = "MozoDelDia";
-            MozoDelDia.Size = new Size(88, 23);
-            MozoDelDia.TabIndex = 3;
-            MozoDelDia.Text = "Mozo del Día";
-            MozoDelDia.UseVisualStyleBackColor = true;
-            // 
-            // btnTotales
-            // 
-            btnTotales.Location = new Point(467, 390);
-            btnTotales.Name = "btnTotales";
-            btnTotales.Size = new Size(75, 23);
-            btnTotales.TabIndex = 4;
-            btnTotales.Text = "Totales";
-            btnTotales.UseVisualStyleBackColor = true;
-            // 
-            // lblImportesVentas
-            // 
-            lblImportesVentas.AutoSize = true;
-            lblImportesVentas.Location = new Point(21, 25);
-            lblImportesVentas.Name = "lblImportesVentas";
-            lblImportesVentas.Size = new Size(161, 15);
-            lblImportesVentas.TabIndex = 5;
-            lblImportesVentas.Text = "Ingrese los importes de venta";
-            // 
-            // lblMozoDelDia
-            // 
-            lblMozoDelDia.AutoSize = true;
-            lblMozoDelDia.Location = new Point(21, 314);
-            lblMozoDelDia.Name = "lblMozoDelDia";
-            lblMozoDelDia.Size = new Size(76, 15);
-            lblMozoDelDia.TabIndex = 6;
-            lblMozoDelDia.Text = "Mozo del Día";
-            // 
-            // lblTotalGeneral
-            // 
-            lblTotalGeneral.AutoSize = true;
-            lblTotalGeneral.Location = new Point(21, 351);
-            lblTotalGeneral.Name = "lblTotalGeneral";
-            lblTotalGeneral.Size = new Size(75, 15);
-            lblTotalGeneral.TabIndex = 7;
-            lblTotalGeneral.Text = "Total General";
-            // 
-            // lblMozoResultado
-            // 
-            lblMozoResultado.AutoSize = true;
-            lblMozoResultado.Location = new Point(112, 314);
-            lblMozoResultado.Name = "lblMozoResultado";
-            lblMozoResultado.Size = new Size(38, 15);
-            lblMozoResultado.TabIndex = 8;
-            lblMozoResultado.Text = "\"...... \"";
-            // 
-            // txtTotalGeneral
-            // 
-            txtTotalGeneral.Location = new Point(112, 348);
-            txtTotalGeneral.Name = "txtTotalGeneral";
-            txtTotalGeneral.ReadOnly = true;
-            txtTotalGeneral.Size = new Size(100, 23);
-            txtTotalGeneral.TabIndex = 9;
             // 
             // colMozos
             // 
@@ -163,23 +92,193 @@
             colPostres.HeaderText = "Postres";
             colPostres.Name = "colPostres";
             // 
+            // btnCerrar
+            // 
+            btnCerrar.Location = new Point(458, 446);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(75, 23);
+            btnCerrar.TabIndex = 1;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = true;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(12, 285);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(75, 23);
+            btnValidar.TabIndex = 2;
+            btnValidar.Text = "Validar Datos";
+            btnValidar.UseVisualStyleBackColor = true;
+            btnValidar.Click += btnValidar_Click;
+            // 
+            // btnMozoDelDia
+            // 
+            btnMozoDelDia.Location = new Point(445, 285);
+            btnMozoDelDia.Name = "btnMozoDelDia";
+            btnMozoDelDia.Size = new Size(88, 23);
+            btnMozoDelDia.TabIndex = 3;
+            btnMozoDelDia.Text = "Mozo del Día";
+            btnMozoDelDia.UseVisualStyleBackColor = true;
+            btnMozoDelDia.Click += btnMozoDelDia_Click;
+            // 
+            // btnTotales
+            // 
+            btnTotales.Location = new Point(98, 285);
+            btnTotales.Name = "btnTotales";
+            btnTotales.Size = new Size(75, 23);
+            btnTotales.TabIndex = 4;
+            btnTotales.Text = "Totales";
+            btnTotales.UseVisualStyleBackColor = true;
+            btnTotales.Click += btnTotales_Click;
+            // 
+            // lblImportesVentas
+            // 
+            lblImportesVentas.AutoSize = true;
+            lblImportesVentas.Location = new Point(12, 5);
+            lblImportesVentas.Name = "lblImportesVentas";
+            lblImportesVentas.Size = new Size(161, 15);
+            lblImportesVentas.TabIndex = 5;
+            lblImportesVentas.Text = "Ingrese los importes de venta";
+            // 
+            // lblMozoDelDia
+            // 
+            lblMozoDelDia.AutoSize = true;
+            lblMozoDelDia.Location = new Point(422, 340);
+            lblMozoDelDia.Name = "lblMozoDelDia";
+            lblMozoDelDia.Size = new Size(76, 15);
+            lblMozoDelDia.TabIndex = 6;
+            lblMozoDelDia.Text = "Mozo del Día";
+            // 
+            // lblTotalGeneral
+            // 
+            lblTotalGeneral.AutoSize = true;
+            lblTotalGeneral.Location = new Point(15, 454);
+            lblTotalGeneral.Name = "lblTotalGeneral";
+            lblTotalGeneral.Size = new Size(75, 15);
+            lblTotalGeneral.TabIndex = 7;
+            lblTotalGeneral.Text = "Total General";
+            // 
+            // lblMozoResultado
+            // 
+            lblMozoResultado.AutoSize = true;
+            lblMozoResultado.Location = new Point(504, 340);
+            lblMozoResultado.Name = "lblMozoResultado";
+            lblMozoResultado.Size = new Size(38, 15);
+            lblMozoResultado.TabIndex = 8;
+            lblMozoResultado.Text = "\"...... \"";
+            // 
+            // txtTotalGeneral
+            // 
+            txtTotalGeneral.Location = new Point(153, 451);
+            txtTotalGeneral.Name = "txtTotalGeneral";
+            txtTotalGeneral.ReadOnly = true;
+            txtTotalGeneral.Size = new Size(100, 23);
+            txtTotalGeneral.TabIndex = 9;
+            // 
+            // lblTotalComidas
+            // 
+            lblTotalComidas.AutoSize = true;
+            lblTotalComidas.Location = new Point(15, 340);
+            lblTotalComidas.Name = "lblTotalComidas";
+            lblTotalComidas.Size = new Size(82, 15);
+            lblTotalComidas.TabIndex = 10;
+            lblTotalComidas.Text = "Total Comidas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 358);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 11;
+            // 
+            // lblBebidasSinAlcohol
+            // 
+            lblBebidasSinAlcohol.AutoSize = true;
+            lblBebidasSinAlcohol.Location = new Point(15, 396);
+            lblBebidasSinAlcohol.Name = "lblBebidasSinAlcohol";
+            lblBebidasSinAlcohol.Size = new Size(111, 15);
+            lblBebidasSinAlcohol.TabIndex = 12;
+            lblBebidasSinAlcohol.Text = "Bebidas Sin Alcohol";
+            // 
+            // lblPostres
+            // 
+            lblPostres.AutoSize = true;
+            lblPostres.Location = new Point(15, 425);
+            lblPostres.Name = "lblPostres";
+            lblPostres.Size = new Size(45, 15);
+            lblPostres.TabIndex = 13;
+            lblPostres.Text = "Postres";
+            // 
+            // lblBebidasConAlcohol
+            // 
+            lblBebidasConAlcohol.AutoSize = true;
+            lblBebidasConAlcohol.Location = new Point(15, 369);
+            lblBebidasConAlcohol.Name = "lblBebidasConAlcohol";
+            lblBebidasConAlcohol.Size = new Size(115, 15);
+            lblBebidasConAlcohol.TabIndex = 14;
+            lblBebidasConAlcohol.Text = "Bebidas con Alcohol";
+            // 
+            // txtTotalComidas
+            // 
+            txtTotalComidas.Location = new Point(153, 337);
+            txtTotalComidas.Name = "txtTotalComidas";
+            txtTotalComidas.ReadOnly = true;
+            txtTotalComidas.Size = new Size(100, 23);
+            txtTotalComidas.TabIndex = 15;
+            // 
+            // txtTotalBebidasCon
+            // 
+            txtTotalBebidasCon.Location = new Point(153, 366);
+            txtTotalBebidasCon.Name = "txtTotalBebidasCon";
+            txtTotalBebidasCon.ReadOnly = true;
+            txtTotalBebidasCon.Size = new Size(100, 23);
+            txtTotalBebidasCon.TabIndex = 16;
+            // 
+            // txtTotalBebidasSin
+            // 
+            txtTotalBebidasSin.Location = new Point(153, 393);
+            txtTotalBebidasSin.Name = "txtTotalBebidasSin";
+            txtTotalBebidasSin.ReadOnly = true;
+            txtTotalBebidasSin.Size = new Size(100, 23);
+            txtTotalBebidasSin.TabIndex = 17;
+            // 
+            // txtTotalPostres
+            // 
+            txtTotalPostres.Location = new Point(153, 422);
+            txtTotalPostres.Name = "txtTotalPostres";
+            txtTotalPostres.ReadOnly = true;
+            txtTotalPostres.Size = new Size(100, 23);
+            txtTotalPostres.TabIndex = 18;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 497);
+            Controls.Add(txtTotalPostres);
+            Controls.Add(txtTotalBebidasSin);
+            Controls.Add(txtTotalBebidasCon);
+            Controls.Add(txtTotalComidas);
+            Controls.Add(lblBebidasConAlcohol);
+            Controls.Add(lblPostres);
+            Controls.Add(lblBebidasSinAlcohol);
+            Controls.Add(label2);
+            Controls.Add(lblTotalComidas);
             Controls.Add(txtTotalGeneral);
             Controls.Add(lblMozoResultado);
             Controls.Add(lblTotalGeneral);
             Controls.Add(lblMozoDelDia);
             Controls.Add(lblImportesVentas);
             Controls.Add(btnTotales);
-            Controls.Add(MozoDelDia);
+            Controls.Add(btnMozoDelDia);
             Controls.Add(btnValidar);
             Controls.Add(btnCerrar);
             Controls.Add(dgvVentas);
             Name = "Form1";
             Text = "Ventas La Milanga";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dgvVentas).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -190,7 +289,7 @@
         private DataGridView dgvVentas;
         private Button btnCerrar;
         private Button btnValidar;
-        private Button MozoDelDia;
+        private Button btnMozoDelDia;
         private Button btnTotales;
         private Label lblImportesVentas;
         private Label lblMozoDelDia;
@@ -202,5 +301,14 @@
         private DataGridViewTextBoxColumn colBebidaSin;
         private DataGridViewTextBoxColumn colBebidas;
         private DataGridViewTextBoxColumn colPostres;
+        private Label lblTotalComidas;
+        private Label label2;
+        private Label lblBebidasSinAlcohol;
+        private Label lblPostres;
+        private Label lblBebidasConAlcohol;
+        private TextBox txtTotalComidas;
+        private TextBox txtTotalBebidasCon;
+        private TextBox txtTotalBebidasSin;
+        private TextBox txtTotalPostres;
     }
 }
--- pryPerezEjercicio4/Form1.cs
+++ pryPerezEjercicio4/Form1.cs
@@ -15,5 +15,206 @@ namespace pryPerezEjercicio4
         // 2. Un array simple para los nombres de los mozos
         //    Lo usaremos para saber el nombre del "Mozo del Día"
         string[] nombresMozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // --- 1. CARGAMOS LAS FILAS DE LA GRILLA ---
+            // Usamos el método .Rows.Add()
+            // El primer valor "Julio" irá a la primera columna (colMozos)
+            // Los siguientes valores ("0") son los valores por defecto para las 4 categorías.
+            dgvVentas.Rows.Add("Julio", "0", "0", "0", "0");
+            dgvVentas.Rows.Add("Esteban", "0", "0", "0", "0");
+            dgvVentas.Rows.Add("Javier", "0", "0", "0", "0");
+            dgvVentas.Rows.Add("Gonzalo", "0", "0", "0", "0");
+            dgvVentas.Rows.Add("Alberto", "0", "0", "0", "0");
+
+            // --- 2. DESHABILITAMOS BOTONES ---
+            // El enunciado pide que los botones de consulta solo se activen
+            // después de presionar "Validar Datos".
+            btnMozoDelDia.Enabled = false;
+            btnTotales.Enabled = false;
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            // --- 1. INICIALIZAMOS LA MATRIZ ---
+            ventas = new float[5, 4];
+
+            // --- 2. CONTROL DE ERRORES (try-catch) ---
+            // Esta es la nueva lógica que permite celdas vacías (las toma como 0)
+            // pero falla si encuentra letras (ej: "abc").
+            try
+            {
+                // --- 3. RECORRIDO DE FILAS (MOZOS) ---
+                for (int f = 0; f < 5; f++)
+                {
+                    // --- 4. RECORRIDO DE COLUMNAS (CATEGORÍAS) ---
+                    for (int c = 1; c < 5; c++)
+                    {
+                        // 5. LECTURA Y CONVERSIÓN (LÓGICA MEJORADA)
+
+                        // Leemos el valor de la celda de forma segura.
+                        string valorCelda = Convert.ToString(dgvVentas.Rows[f].Cells[c].Value);
+
+                        // Declaramos 'importe' y le damos un valor por defecto de 0.
+                        float importe = 0;
+
+                        // AHORA PREGUNTAMOS: ¿La celda NO está vacía?
+                        if (valorCelda != "")
+                        {
+                            // Si NO está vacía (ej: "150" o "abc"),
+                            // intentamos convertirla a número.
+                            // Si tiene "abc", ESTA LÍNEA FALLA y salta al 'catch'.
+                            importe = float.Parse(valorCelda);
+                        }
+                        // Si la celda SÍ estaba vacía, 'importe' se queda con el valor 0.
+
+                        // 6. GUARDADO EN LA MATRIZ
+                        ventas[f, c - 1] = importe;
+                    }
+                }
+
+                // --- 7. VALIDACIÓN EXITOSA ---
+                MessageBox.Show("Datos validados correctamente. (Celdas vacías se tomaron como 0)");
+
+                // HABILITAMOS los botones
+                btnMozoDelDia.Enabled = true;
+                btnTotales.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                // --- 8. ERROR DE VALIDACIÓN ---
+                // El 'try' falló (ej: una celda con "abc").
+                MessageBox.Show("Error: Verifique que todas las celdas tengan un valor numérico. (No se permiten letras).");
+
+                // Nos aseguramos de que los botones sigan deshabilitados.
+                btnMozoDelDia.Enabled = false;
+                btnTotales.Enabled = false;
+            }
+        }
+
+        private void btnMozoDelDia_Click(object sender, EventArgs e)
+        {
+            // --- 1. PREPARAMOS LAS VARIABLES ---
+
+            // Una variable para guardar el total de venta MÁS ALTO que encontremos.
+            // La inicializamos en -1 (o un número bajo) para que la primera venta (ej: $100)
+            // sea sí o sí más alta que este valor.
+            float maximoVenta = -1;
+
+            // Una variable para guardar el ÍNDICE (la posición 0, 1, 2, 3 o 4)
+            // del mozo que tenga la venta más alta.
+            int indiceMozoMaximo = 0;
+
+            // --- 2. RECORRIDO DE FILAS (MOZOS) ---
+            // Este 'for' (con la 'f' de fila) dará 5 vueltas (0 a 4), una por cada mozo.
+            for (int f = 0; f < 5; f++)
+            {
+                // 3. CÁLCULO DEL TOTAL POR MOZO
+                // Cada vez que empezamos a revisar un mozo nuevo (fila nueva),
+                // ponemos su "calculadora" personal en cero.
+                float totalMozoActual = 0;
+
+                // 4. RECORRIDO DE COLUMNAS (CATEGORÍAS)
+                // Este 'for' (con la 'c' de columna) dará 4 vueltas (0 a 3)
+                // para sumar las 4 categorías del mozo actual (fila 'f').
+                for (int c = 0; c < 4; c++)
+                {
+                    // Sumamos lo que hay en la matriz en [f, c] a la calculadora del mozo
+                    // Ej: ventas[0, 0] (Julio, Comida)
+                    //     ventas[0, 1] (Julio, BebidaSin)
+                    //     ...
+                    totalMozoActual = totalMozoActual + ventas[f, c];
+                }
+
+                // 
+
+                // --- 5. COMPARACIÓN ---
+                // Cuando el 'for' de las columnas termina, ya tenemos el total de ese mozo (totalMozoActual).
+                // Ahora, lo comparamos con el máximo que teníamos guardado.
+                if (totalMozoActual > maximoVenta)
+                {
+                    // ¡Encontramos un nuevo ganador!
+                    // 1. Guardamos su venta como el nuevo máximo.
+                    maximoVenta = totalMozoActual;
+
+                    // 2. Guardamos su ÍNDICE (la fila 'f') como el índice del ganador.
+                    indiceMozoMaximo = f;
+                }
+            }
+
+            // --- 6. MOSTRAR RESULTADOS ---
+            // Cuando el 'for' de las filas (mozos) termina, las variables 
+            // 'maximoVenta' e 'indiceMozoMaximo' tienen los datos del ganador final.
+
+            // Buscamos el nombre del ganador en nuestro array 'nombresMozos'
+            // usando el índice (posición) que guardamos.
+            string nombreGanador = nombresMozos[indiceMozoMaximo];
+
+            // Mostramos el resultado en el Label 'lblMozoResultado'.
+            // El '$' al inicio nos deja poner variables { } dentro del texto.
+            // 'maximoVenta.ToString("0.00")' formatea el número para que muestre 2 decimales.
+            lblMozoResultado.Text = $"{nombreGanador} (Total: ${maximoVenta.ToString("0.00")})";
+        }
+
+        private void btnTotales_Click(object sender, EventArgs e)
+        {
+            // --- 1. PREPARAMOS LAS VARIABLES ---
+
+            // Una variable para la suma de TODO.
+            float totalGeneral = 0;
+
+            // Un array de 4 espacios para guardar las sumas de cada CATEGORÍA.
+            // Posición 0: Total Comida
+            // Posición 1: Total Bebida Sin Alcohol
+            // Posición 2: Total Bebidas con Alcohol
+            // Posición 3: Total Postres
+            float[] totalesCategoria = new float[4];
+
+            // --- 2. RECORRIDO DE FILAS (MOZOS) ---
+            for (int f = 0; f < 5; f++)
+            {
+                // --- 3. RECORRIDO DE COLUMNAS (CATEGORÍAS) ---
+                // (c=0 es Comida, c=1 es BebidaSin, c=2 es BebidasCon, c=3 es Postres)
+                for (int c = 0; c < 4; c++)
+                {
+                    // 4a. SUMA PARA EL TOTAL GENERAL
+                    // Leemos el valor (ej: ventas[0,0]) y lo sumamos a la bolsa grande.
+                    totalGeneral = totalGeneral + ventas[f, c];
+
+                    // 4b. SUMA PARA EL TOTAL DE LA CATEGORÍA
+                    // Sumamos el mismo valor a la posición que le corresponde.
+                    // Ej: ventas[0, 0] (Julio, Comida) se suma a totalesCategoria[0]
+                    //     ventas[0, 1] (Julio, BebidaSin) se suma a totalesCategoria[1]
+                    totalesCategoria[c] = totalesCategoria[c] + ventas[f, c];
+                }
+            }
+
+            // --- 5. MOSTRAR RESULTADOS EN LOS TEXTBOX ---
+
+            // Formateamos los números a 2 decimales y con el signo '$'
+
+            // Asignamos el total de la categoría "Comida" (índice 0)
+            txtTotalComidas.Text = $"${totalesCategoria[0].ToString("0.00")}";
+
+            // Asignamos el total de "Bebida Sin Alcohol" (índice 1)
+            txtTotalBebidasSin.Text = $"${totalesCategoria[1].ToString("0.00")}";
+
+            // Asignamos el total de "Bebidas con Alcohol" (índice 2)
+            txtTotalBebidasCon.Text = $"${totalesCategoria[2].ToString("0.00")}";
+
+            // Asignamos el total de "Postres" (índice 3)
+            txtTotalPostres.Text = $"${totalesCategoria[3].ToString("0.00")}";
+
+            // Asignamos el Total General
+            txtTotalGeneral.Text = $"${totalGeneral.ToString("0.00")}";
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            // 'this' se refiere a este formulario (Form1)
+            // '.Close()' es la orden de cerrarse.
+            this.Close();
+        }
     }
 }
